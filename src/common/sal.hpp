9d500ab87c017830ff006284b771788fa6dd92cd
Arthur <arajala@umich.edu>
2014-12-11 21:12:55 -0500

    i2c sal defs

--- src/common/sal.hpp
+++ src/common/sal.hpp
@@ -1,4 +1,8 @@
 #ifndef SAL_HPP
 #define SAL_HPP
 
+uint8_t SAL_i2c_read(uint8_t slave_address, uint8_t reg_address);
+
+void SAL_i2c_write(uint8_t slave_address, uint8_t reg_address, uint8_t data);
+
 #endif // SAL_HPP
